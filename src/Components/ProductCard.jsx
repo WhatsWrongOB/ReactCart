970a2c305d6007123b791801678e095d46303b2c
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2023-11-26 12:42:51 +0500

    first commit

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -5,6 +5,7 @@ const ProductCard = ({ product, addToCart }) => {
     <div className="card">
       <img src={product.image} alt="img" />
       <h2 className="heading">{product.title}</h2>
+      <h4>ratings {product.rating.rate}</h4>
       <h2 className="heading_two">${product.price}</h2>
       <button className='btn' onClick={() => { addToCart(product) }}>Add To Cart</button>
     </div>
